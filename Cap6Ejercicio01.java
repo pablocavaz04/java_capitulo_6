b5ef066e6130fe226c05caf81ee74b72e32c0e90
pablocavaz04 <camvazpablo04@gmail.com>
2022-11-09 23:13:04 +0100

    First Commit

--- Cap6Ejercicio01.java
+++ Cap6Ejercicio01.java
@@ -1,11 +1,32 @@
 /*
- * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+ * Escribe un programa que muestre la tirada de tres dados. Se debe mostrar
+también la suma total (los puntos que suman entre los tres dados).
  * Author:Pablo Camino Vázquez
  */
+import java.util.Scanner;
 public class Cap6Ejercicio01 {
     public static void main(String[] args) {
-        for (int i=5; i<=100; i+=5){
-            System.out.println(i);
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Si quieres empezar el juego pulse 1(yes)/cualquier otra tecla(no): ");
+        int yes_no = sc.nextInt();
+        if (yes_no == 1){
+            int i = 0;
+            int suma = 0;
+            do{
+                i++;
+                System.out.print("Tire el "+i+" dado, pulse 2: ");
+                int start = sc.nextInt();
+                if(start == 2){
+                    int num_dado = (int)(Math.random()*6)+1;
+                    System.out.println(num_dado);
+                    suma+=num_dado;
+                }else {
+                    System.out.println("FIN DEL JUEGO ");
+                }
+            }while(i < 3);
+            System.out.println("La suma total es de : "+suma);
+        }else {
+            System.out.println(" FIN DEL JUEGO");
+        }  
     }
 }
