b43f249cd54a13a9800336cece1c239baca0da30
pablocavaz04 <camvazpablo04@gmail.com>
2022-11-09 17:48:17 +0100

    First Commit

---
+++ Cap6Ejercicio01.java
@@ -0,0 +1,11 @@
+/*
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+ * Author:Pablo Camino Vázquez
+ */
+public class Cap6Ejercicio01 {
+    public static void main(String[] args) {
+        for (int i=5; i<=100; i+=5){
+            System.out.println(i);
+        }
+    }
+}
