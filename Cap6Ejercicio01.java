7fd83d53958d7e4df2c7c03be4f73a3556461218
pablocavaz04 <camvazpablo04@gmail.com>
2022-11-09 23:21:02 +0100

    First Commit

--- Cap6Ejercicio01.java
+++ Cap6Ejercicio01.java
@@ -1,7 +1,7 @@
 /*
- * Escribe un programa que muestre la tirada de tres dados. Se debe mostrar
-también la suma total (los puntos que suman entre los tres dados).
  * Author:Pablo Camino Vázquez
+1) Escribe un programa que muestre la tirada de tres dados. Se debe mostrar
+también la suma total (los puntos que suman entre los tres dados).
  */
 import java.util.Scanner;
 public class Cap6Ejercicio01 {
---
+++ Cap6Ejercicio02.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Pablo Camino Vázquez
+Realiza un programa que muestre al azar el nombre de una carta de la baraja
+francesa. Esta baraja está dividida en cuatro palos: picas, corazones, diamantes
+y tréboles. Cada palo está formado por 13 cartas, de las cuales 9 cartas son
+numerales y 4 literales: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K y A (que sería el 1). Para convertir un número en una cadena de caracteres podemos usar String.valueOf(n).
+ */
+import java.util.Scanner;
+public class Cap6Ejercicio02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
