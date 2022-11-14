ef8e679946067031814ffde12610657f1978e282
pablocavaz04 <camvazpablo04@gmail.com>
2022-11-14 10:29:35 +0100

    First Commit

---
+++ Cap6Ejercicio11.java
@@ -0,0 +1,19 @@
+/*
+ * Author:Pablo Camino Vázquez
+11) Escribe un programa que muestre 20 notas generadas al azar. Las notas deben
+aparecer de la forma: suspenso, suficiente, bien, notable o sobresaliente. Al
+final aparecerá el número de suspensos, el número de suficientes, el número
+de bienes, etc.
+ */
+public class Cap6Ejercicio11 {
+    int suspenso=0;
+    int suficiente=0;
+    int bien=0;
+    int notable=0;
+    int sobresaliente=0;
+    public static void main(String[] args) {
+    for(int i=1;i<=20;i++){
+        int nota=(int)(Math.random()*21);
+    }
+    }
+}
