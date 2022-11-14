2b86fa9864583b8771d0f0e4438532d48726a622
pablocavaz04 <camvazpablo04@gmail.com>
2022-11-14 09:11:43 +0100

    First Commit

--- Cap6Ejercicio07.java
+++ Cap6Ejercicio07.java
@@ -5,6 +5,32 @@ para los 14 partidos y el pleno al quince (15 filas).
  */
 public class Cap6Ejercicio07 {
     public static void main(String[] args) {
-        
+        int fila = 15;
+        int numeroquiniela = 0;
+        int columna = 3;
+
+        for(int i = 1; i <= fila; i++){
+                System.out.printf(" %5d  |", i);
+            if(i==15){
+                columna = 1;
+            }
+
+            for(int b = 1 ; b <= columna ; b++){
+                numeroquiniela = (int)(Math.random()*3+1);
+                switch (numeroquiniela) {
+                    case 1:
+                        System.out.print(" 1 |");
+                    break;
+                    case 2:
+                        System.out.print(" x |");
+                    break;
+                    case 3:
+                        System.out.print(" 2 |");
+                    break;
+                }
+            }
+
+            System.out.println("");
+        }
     }
 }
