986b7d60fcec3e5c7646c793b2b94afc63afac0e
pablocavaz04 <camvazpablo04@gmail.com>
2022-11-14 21:45:15 +0100

    First Commit

--- Cap6Ejercicio11.java
+++ Cap6Ejercicio11.java
@@ -6,14 +6,54 @@ final aparecerá el número de suspensos, el número de suficientes, el número
 de bienes, etc.
  */
 public class Cap6Ejercicio11 {
-    int suspenso=0;
-    int suficiente=0;
-    int bien=0;
-    int notable=0;
-    int sobresaliente=0;
     public static void main(String[] args) {
+        int nota= 0;
+        int suspenso=0;
+        int suficiente=0;
+        int bien=0;
+        int notable=0;
+        int sobresaliente=0;
     for(int i=1;i<=20;i++){
-        int nota=(int)(Math.random()*21);
-    }
+        nota=(int)(Math.random()*11);
+        switch(nota){
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            suspenso ++;
+            System.out.println("SUSPENSO");
+            System.out.println();
+            break;
+            case 5:
+            suficiente ++;
+            System.out.println("SUFICIENTE");
+            System.out.println();
+            break;
+            case 6:
+            bien ++;
+            System.out.println("BIEN");
+            System.out.println();
+            break;
+            case 7:
+            case 8:
+            notable ++;
+            System.out.println("NOTABLE");
+            System.out.println();
+            break;
+            case 9:
+            case 10:
+            sobresaliente ++;
+            System.out.println("SOBRESALIENTE");
+            System.out.println();
+            break;
+            }
+        }
+        System.out.println("La cantidad de suspensos es: "+suspenso);
+        System.out.println("La cantidad de suficiente es de :"+suficiente);
+        System.out.println("La cantidad de bienes es de : "+bien);
+        System.out.println("La cantidad de notables es de : "+notable);
+        System.out.println("La cantidad de sobresalientes es de : "+sobresaliente);
+
     }
 }
