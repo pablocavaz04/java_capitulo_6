5c46e9ff8b97227ab69adb8f4c8bb0642deb8b5c
pablocavaz04 <camvazpablo04@gmail.com>
2022-11-14 09:09:02 +0100

    First Commit

--- Cap6Ejercicio07.java
+++ Cap6Ejercicio07.java
@@ -1,3 +1,8 @@
+/*
+ * Author:Pablo Camino Vázquez
+7) Escribe un programa que muestre tres apuestas de la quiniela en tres columnas
+para los 14 partidos y el pleno al quince (15 filas).
+ */
 public class Cap6Ejercicio07 {
     public static void main(String[] args) {
         
